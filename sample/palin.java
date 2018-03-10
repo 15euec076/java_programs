621ee106f42d733ae9123ee846ce92fe6e5c3b94
15euec076 <32997235+15euec076@users.noreply.github.com>
2018-03-10 15:59:51 +0530

    Create palin.java

---
+++ sample/palin.java
@@ -0,0 +1,29 @@
+package info;
+import java.util.Scanner;
+
+public class Palin {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		int rem,rev=0;
+		int temp = num;
+		while(num>0)
+		{
+			rem = num%10;
+			rev = rev*10 + rem;
+			num = num/10;
+		}
+		if(temp==rev)
+		{
+			System.out.println("palindrome");
+		}
+		else
+		{
+			System.out.println("not palindrome");
+		}
+				
+		
+	}
+
+}
