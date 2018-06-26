c3376ecc28c25dca0d8000a7673c0f2ee72df818
15euec076 <32997235+15euec076@users.noreply.github.com>
2018-06-26 12:27:24 +0530

    Create toggle.java

---
+++ sample/toggle.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		String in;
+		in = s.next();
+		char [] c = in.toCharArray();
+		int i,temp;
+		for(i=0;i<c.length;i++)
+		{
+			if(c[i]>=65 && c[i]<=97)
+			{
+			 temp = (int) c[i];
+			 temp = temp +32;
+			 c[i] = (char) temp;
+			}
+			else
+			{
+				temp = (int) c[i];
+			 temp = temp -32;
+			 c[i] = (char) temp;
+				
+			}
+		}
+		for(i=0;i<c.length;i++)
+		{
+			System.out.print(c[i]);
+		}
+		
+	}
+}
